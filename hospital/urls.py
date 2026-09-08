1e3d3420efdda1e7afad307520edb972d9d2171b
Aliga Darin Ubaoma <daroaliga@gmail.com>
2026-09-08 19:03:48 +0100

    Adding Url

---
+++ hospital/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('doctors/', views.doctor_list, name='doctor_list'),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('book-appointment/', views.book_appointment, name='book_appointment'),
+]
