cf47bc3b1e250bc1dab4601d7ec491769040b884
Aliga Darin Ubaoma <daroaliga@gmail.com>
2026-09-09 16:53:39 +0100

    Creating Doctor Dashboard URL

--- hospital/urls.py
+++ hospital/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('book-appointment/', views.book_appointment, name='book_appointment'),
     path( 'doctor/<int:id>/', views.doctor_profile, name='doctor_profile'),
+    path( 'doctor-dashboard/', views.doctor_dashboard, name='doctor_dashboard'),
 ]
