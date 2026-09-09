0c42f2d345114e04a9235ab7596e53981be4f945
Aliga Darin Ubaoma <daroaliga@gmail.com>
2026-09-09 07:54:09 +0100

    Admin Setup

--- hospital/admin.py
+++ hospital/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Department, Doctor, Patient, Appointment
 
-# Register your models here.
+admin.site.register(Department)
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(Appointment)
