7035d9540497d8be3ec4d0356cf06fc9235440aa
Aliga Darin Ubaoma <daroaliga@gmail.com>
2026-09-09 13:39:19 +0100

    Improving the Appointment Model

--- hospital/models.py
+++ hospital/models.py
@@ -110,6 +110,17 @@ class Appointment(models.Model):
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
     created_at = models.DateTimeField(auto_now_add=True)
 
+    appointment_number = models.CharField(
+    max_length=20,
+    unique=True,
+    blank=True
+    )
+
+
+    notes = models.TextField(
+        blank=True
+    )
+    
     def __str__(self):
         return f"{self.patient} with Dr. {self.doctor} on {self.date}"
 
