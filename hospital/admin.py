823287727a2c4ad8aa9dca937a90982601823bc3
Aliga Darin Ubaoma <daroaliga@gmail.com>
2026-09-09 10:06:13 +0100

    Updating the Admin Panel

--- hospital/admin.py
+++ hospital/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 from .models import Department, Doctor, Patient, Appointment
 
 admin.site.register(Department)
-admin.site.register(Doctor)
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+        'department',
+        'experience',
+        'rating',
+    )
+
+    search_fields = (
+        'name',
+        'department__name',
+    )
+
+    list_filter = (
+        'department',
+    )
 admin.site.register(Patient)
 admin.site.register(Appointment)
